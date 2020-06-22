25eca7da763fd8f93c58b8b14038e87e5ea027ef
Kumiko Yamazaki <kyamazaki@gmail.com>
2020-06-21 18:01:50 -0600

    moving Find a User to separate folder

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Hot from '../hot/Hot.js';
-import FindUser from './FindUser.js';
+import FindUser from '../findUser/FindUser.js';
 import './Home.css';
 
 class Home extends React.Component {
